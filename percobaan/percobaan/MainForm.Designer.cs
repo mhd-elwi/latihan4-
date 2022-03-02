56655456cf0a5578f63ac51b285d34ec09bfd09b
mhd elwi <85838020+mhd-elwi@users.noreply.github.com>
2022-03-02 16:02:51 +0700

    Add files via upload

---
+++ percobaan/percobaan/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 02/03/2022
+ * Time: 15:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace percobaan
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(121, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "nama";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(196, 38);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(120, 20);
+			this.textBox1.TabIndex = 1;
+			
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 74);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(121, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "nim";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 106);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(121, 23);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "kelas";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(13, 146);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(121, 23);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "mata kuliah";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(196, 76);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(120, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(196, 108);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(120, 20);
+			this.textBox3.TabIndex = 6;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(196, 148);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(120, 20);
+			this.textBox4.TabIndex = 7;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(105, 199);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(92, 23);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "klik ";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(322, 293);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "percobaan";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
